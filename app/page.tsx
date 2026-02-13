8edeaaefe844a7d6574bc7226590cdfccc4c3e37
Michael <m.zakka@tgmena.com>
2026-02-13 18:44:56 +0300

    projects

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         <Services />
         <Experience />
         <Skills />
-        <Projects />
+        {/* <Projects /> */}
         <Education />
         <Contact />
       </main>
